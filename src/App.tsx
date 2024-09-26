a4920f10f1c915cec3a9799b9ce5155ece0d2ceb
Diego <diannewdev@gmail.com>
2024-09-25 20:20:36 -0400

    template basico do form foi feito

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App: React.FC = () => {
-  return (
-    <h1>Hello world</h1>
+
+    const [answer, setAnswer] = useState<string>('');
+
+    function handleInput(e:React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(e.target.value)        
+    }
+
+    function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+        console.log(answer) //esse é o que nos interessa
+    }
+    
+    return (
+        <div className='grid place-items-center'>
+            <div className='bg-blue-950 w-full h-full'>
+                <div>
+                    <h1>CONTADOR DE TECHS</h1>
+                </div>
+
+                <div>
+                    <form name='formulario' onSubmit={handleSubmit} className='flex gap-4'>
+                        
+                        <input onChange={handleInput} name='texto'  type="text" placeholder='Tech' />
+                        <button type='submit'>Submeter</button>
+                        
+                        <button type='reset'>Limpar</button>
+                    </form>
+
+                <div>
+
+                </div>
+
+
+                    
+                </div>
+                
+                
+            </div>
+        </div>
   );
 };
 
